bfd85a37c429b12f3ed0adb6f7833dae3b9965f2
FREDERIC TICON <fred.ticon@gmail.com>
2023-10-23 12:22:01 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,66 @@
+const express = require("express");
+
+const app = express();
+
+const port = 3001;
+
+app.get("/", (req, res) => {
+    res.send("Welcome to my favourite movie list");
+  });
+
+app.get("/api/movies", (req, res) => {
+    res.status(200).json(movies);
+  });
+
+  
+  
+  
+  app.get("/api/movies/:id", (req, res) => {
+    res.send(`Welcome ${req.params.id}`);
+  });
+  app.get("/users/:id", );
+
+
+
+
+
+
+
+
+
+
+
+  const movies = [
+  {
+    id: 1,
+    title: "Citizen Kane",
+    director: "Orson Wells",
+    year: "1941",
+    color: false,
+    duration: 120,
+  },
+  {
+    id: 2,
+    title: "The Godfather",
+    director: "Francis Ford Coppola",
+    year: "1972",
+    color: true,
+    duration: 180,
+  },
+  {
+    id: 3,
+    title: "Pulp Fiction",
+    director: "Quentin Tarantino",
+    year: "1994",
+    color: true,
+    duration: 180,
+  },
+];
+
+app.listen(port, (err) => {
+  if (err) {
+    console.error("Something bad happened");
+  } else {
+    console.log(`Server is listening on ${port}`);
+  }
+});
