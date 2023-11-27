03d68f3246038ee418f51583c2793727cd910259
FREDERIC TICON <fred.ticon@gmail.com>
2023-11-27 16:23:32 +0100

    Update movies.test.js

--- tests/movies.test.js
+++ tests/movies.test.js
@@ -1,2 +1,21 @@
 const request = require("supertest");
 const app = require("../app");
+
+describe("GET /api/movies", () => {
+  it("should return all movies", async () => {
+    /** Code your test here */
+    const response = await request(app).get("/api/movies");
+
+    expect(response.headers["content-type"]).toMatch(/json/);
+    expect(response.status).toEqual(200);
+  });
+});
+describe("GET /api/movies/:id 1", () => {
+  it("should return all movies", async () => {
+    /** Code your test here */
+    const response = await request(app).get("/api/movies/:id 1");
+
+    expect(response.headers["content-type"]).toMatch(/json/);
+    expect(response.status).toEqual(200);
+  });
+});
